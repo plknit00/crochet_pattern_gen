64bfca7a1f5f039ad2d0b21ef266a14a77cd9910
Paige Knittel <plknit00@gmail.com>
2025-03-31 22:38:22 -0700

    restarted

--- src/crochet_pattern.cc
+++ src/crochet_pattern.cc
@@ -109,35 +109,30 @@ void CrochetPattern::sphere_print(int num_rows) {
   std::cout << "Num rows " << num_rows << std::endl;
   int sc_mr = 6;
   for (int row = 0; row < num_rows; row++) {
-    std::cout << "math= " << ((3 * (row + 1)) / num_rows) << std::endl;
     if (row == 0) {  // first row
-      // std::cout << "FR" << std::endl;
       std::cout << "Round " << row + 1 << ": mr " << sc_mr << " [" << sc_mr
                 << "]" << std::endl;
     } else if (row == 1) {  // second row
-      // std::cout << "SR" << std::endl;
       std::cout << "Round " << row + 1 << ": inc x " << sc_mr << " ["
                 << 2 * sc_mr << "]" << std::endl;
     } else if (row == num_rows - 1) {  // last row
-      // std::cout << "LR" << std::endl;
       std::cout << "Round " << row + 1 << ": dec x " << sc_mr << " [" << sc_mr
                 << "]" << std::endl;
-    } else if (((3 * (row + 1)) / num_rows) <= 1) {  // beginning third
-      // std::cout << "BT" << std::endl;
+    } else if ((3 * ((row + 1) / static_cast<double>(num_rows))) <=
+               1) {  // beginning third
       int num_stiches = sc_mr * (row + 1);
       std::cout << "Round " << row + 1 << ": (sc " << (row - 1) << ", inc)x"
                 << sc_mr << " [" << num_stiches << "]" << std::endl;
-    } else if (((3 * (row + 1)) / num_rows) <= 2) {  // middle third
-      // std::cout << "MT" << std::endl;
-      int num_stiches = sc_mr * (row + 1);
+    } else if ((3 * ((row + 1) / static_cast<double>(num_rows))) <=
+               2) {  // middle third
+      int num_stiches = sc_mr * row;
       int num_rows_repeat = num_rows / 3;
-      std::cout << "Rounds " << row + 1 << "-" << row + num_rows_repeat + 1
-                << ": (" << num_rows_repeat << " rounds) sc around ["
-                << num_stiches << "]" << std::endl;
-      row += num_rows;
+      std::cout << "Rounds " << row + 1 << "-" << row + num_rows_repeat << ": ("
+                << num_rows_repeat << " rounds) sc around [" << num_stiches
+                << "]" << std::endl;
+      row += num_rows_repeat;
     } else {  // final third
-      // std::cout << "FT " << std::endl;
-      int num_stiches = sc_mr * (row + 1);
+      int num_stiches = sc_mr * (num_rows - row);
       std::cout << "Round " << row + 1 << ": (sc " << (num_rows - row)
                 << ", dec)x" << sc_mr << " [" << num_stiches << "]"
                 << std::endl;
